3354d0fa145b988c0218b248af2d6d45e40ea7e5
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-03-07 09:58:07 -0500

    Add challenge 5

---
+++ 05.py
@@ -0,0 +1,8 @@
+'''
+Given a sequence of numbers from standard input separated by spaces, print them out in reverse order.
+E.g., given the input "1 2 3 4 5", your program should print out: "5 4 3 2 1"
+
+Note: Do not print anything inside `input()`; the way the check script checks your output, it must only output the answer.
+'''
+
+
