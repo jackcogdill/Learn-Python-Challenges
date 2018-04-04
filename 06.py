d3264b7270a59166c5e692675bf1914577b0e190
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-04-04 00:10:58 -0400

    Add challenge 6: dictionaries

---
+++ 06.py
@@ -0,0 +1,31 @@
+# Translator! Use the following dictionary to
+# translate a given sentence into leetspeak.
+#
+# Reference: https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+#
+# Rules: if the word exists, use the word. Otherwise,
+# Only translate character by character (if it exists in the dictionary).
+# E.g., 'but my leet senses were troubled' => 'bu7 my 1337 53n535 w3|23 7|20ub13[)'
+
+table = {
+    'more': 'm04R',
+    'fear': 'ph33r',
+    'hacker': 'h4x0r',
+    'has': 'h4z',
+    'have': 'h4z',
+    'you': 'j00',
+    'the': 't3h',
+    'please': 'plz',
+    'I': 'EyE',
+    'o': '0',
+    'e': '3',
+    't': '7',
+    'l': '1',
+    'i': '1',
+    's': '5',
+    'g': '6',
+    'c': '(',
+    'd': '[)',
+    'r': '|2',
+}
+
