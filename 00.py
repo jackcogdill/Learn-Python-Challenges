677a8d25e55981fb7ed79165946653a6dfb8b900
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-04-03 23:43:01 -0400

    Add challenge 0 (hello world)

---
+++ 00.py
@@ -0,0 +1,8 @@
+# Mess around with the following program!
+#
+# Hint: try to make the program ask the user's name,
+# and then reply with a specialized message! E.g., Hello, Bob!
+# For reference: https://docs.python.org/3/library/functions.html#input
+
+print('Hello, World')
+
